d1c1eeeb92b948e592660fa7dae9f8c0a382482f
masseda <masse.dany@gmail.com>
2014-04-25 16:43:42 +0200

    ajout du main correspondant

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Person.hpp"
 #include "Group.hpp"
 #include "Donor.hpp"
+#include "Csv.hpp"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -44,101 +45,29 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	/********* LOCAL VARIABLES *********/
-    string name, phone, gn, tmp, currentLine, item;
-    bool group_status;
 
-    int nbPersPerGroup = 0, lineCounter = 0, nbDonor=0, color_indexer = 0;
+
+    int color_indexer = 0;
     float aExpensesPerPerson;
 
-    vector<string>  list_group;
-    vector<Person*> vPerson;
     vector<Group> Groups;
 
     const char* colors[NB_MAX_COLOR] = {BOLDWHITE, BOLDRED, BOLDGREEN, BOLDBLUE, BOLDMAGENTA, BOLDCYAN, BOLDBLACK};
 	/*---------------------------------*/
-    if(argv[1] == NULL)
+
+    Csv csv;
+    if (argv[1] == NULL)
     {
     	cout << "\nNo input file specified... Exiting" << endl;
-    	return 1;
+    	return 0;
     }
-    cout << "\nInput data file : " << argv[1] << endl;
-    ifstream myFile;
-    Person *aPerson;
-    try
-    {
-    	myFile.open(argv[1]);
-    	if(!myFile) myFile.exceptions(ifstream::failbit); // Set flag fail bit for exceptions
-    }
-    catch(ios_base::failure &e)
-    {
-    	cout << "Exception caught while opening your file : " << endl;
-    	cout << e.what() << endl;
-    }
-    while(1)
-    {
+    csv.readCSV(string(argv[1]));
+    csv.createGroup(Groups);
 
-    	getline(myFile, currentLine);
-    	if(lineCounter != 0)
-    	{
-    		if(myFile.eof()) break;
-    		stringstream ss(currentLine.c_str());
-    		vector<string> person;
-    		while (std::getline(ss, item, ','))
-    		{
-    			person.push_back(item);
-    		}
-//    		Person *aPerson = new Person;
-    		/* TODO modifier boucle if + déclaration *person *donor */
-    		if(person[4] == "Person")
-    		{
-    			aPerson = new Person;
-    			aPerson->setName(person[0]);
-    			aPerson->setPhoneNumber(person[1]);
-    			aPerson->setExpenses(atof(person[2].c_str()));
-    			aPerson->setGroupName(person[3]);
-    			aPerson->setType();
-    		}
-//    		Donor *aDonor = new Donor;
-    		if (person [4] == "Donor")
-    		{
-    			aPerson = new Donor;
-    			aPerson->setName(person[0]);
-    			aPerson->setPhoneNumber(person[1]);
-    			aPerson->setExpenses(atof(person[2].c_str()));
-    			aPerson->setGroupName(person[3]);
-    			aPerson->setType();
-    		}
-    		if(list_group.size() == 0)
-    			list_group.push_back(person[3]);
-    		for(unsigned int i=0; i<list_group.size(); i++)
-    		{
-    			if(list_group[i] == person[3]) //group existe deja
-    			{
-    				group_status = true;
-    				break;
-    			}
-    			else
-    				group_status = false;
-    		}
-    		if(!group_status)
-    		{
-    			list_group.push_back(person[3]); // contient le nom des groupes
-    		}
-//    		if(person[4] == "Person")
-    			vPerson.push_back(aPerson); // contient toutes les personnes
-//    		else
-//    		{
-//    			Person* transDonor = dynamic_cast<Person*>(aDonor);
-//    			vPerson.push_back(aDonor);
-//    		}
-    	}
-    	lineCounter++;
-
-    }
 #ifdef DEBUG
     for(int i=0; i<vPerson.size();i++)
     {
-    	cout << vPerson[i].getName() << vPerson[i].getPhoneNumber() << endl;
+    	cout << vPerson[i]->getName() << vPerson[i]->getPhoneNumber() << endl;
 
     }
 
@@ -146,35 +75,12 @@ int main(int argc, char **argv)
     	cout << list_group[i] << endl;
 #endif
 
-    //trier les donnees lues dans le csv
-    for(unsigned int i=0; i<list_group.size(); i++)
-    {
-    	Group aGroup;
-    	aGroup.setGroupName(list_group[i]);
-    	for(unsigned int j=0; j<vPerson.size(); j++)
-    	{
-    		if(list_group[i] == vPerson[j]->getGroupName())
-    		{
-    			//personne par groupe
-    			nbPersPerGroup++;
-    			aGroup.push_back(vPerson[j]);
-    			if(vPerson[j]->getType() == "Donor")
-    			{
-    				nbDonor++;
-    			}
-    		}
-    	}
-    	//remplir vecteur de group
-    	aGroup.setNbPersGroup(nbPersPerGroup-nbDonor);
-    	nbPersPerGroup = 0;
-    	nbDonor = 0;
-    	Groups.push_back(aGroup);
-    }
-
     /*
      *  Prepare the output
      */
+
     cout << endl;
+
     for(unsigned int i=0; i<Groups.size(); i++)
     {
     	cout << "Total expenses for group " << BOLDBLUE << Groups[i].getGroupName() << " : " << BOLDRED << Groups[i].totalExpenses() << RESET <<endl;
@@ -182,10 +88,6 @@ int main(int argc, char **argv)
     	cout << "Average expenses per person : " << BOLDYELLOW << aExpensesPerPerson << RESET << endl;
     	cout << endl;
     }
-
-//    cout << BOLDWHITE <<"Name\t\t" << "Phone Number\t" << "Expenses\t"
-//
-//    		<< "Payback\t" << "Group\t\t" << "Type" << RESET << endl;
     cout << setw(0) << left
         			 <<	BOLDWHITE << setw(16) << left
         		     << "Name" << setw(16) << left
