9ffd184614b6da506630ea87067de22664060779
stephqn <shadowmen2@hotmail.com>
2014-05-12 14:32:55 +0200

    -Ajout des tests sur toutes les possibilités du case Arg

--- tests/ArgTest.cpp
+++ tests/ArgTest.cpp
@@ -20,12 +20,19 @@ string newdata = "Empty";
 char *argvRead[2];
 char *argvWrite[12];
 char *argvError[1];
+char *argvErrorNoFile[2];
+char *argvErrorHelpAsked[1];
+char *argvErrorPhoneOnly[1];
 
 Arg ReadArgTest;
 Arg WriteArgTest;
 Arg ErrorArgTest;
+Arg ErrorSansArg;
+Arg ErrorNoFileMentionned;
+Arg ErrorHelpAsked;
+Arg ErrorPhoneOnly;
 
-TEST(Arg, isReadOk)
+TEST(Arg, isReadOk)//Test avec 3 arguments correct lecture fichier
 {
 	argvRead[1] = "--file";
 	argvRead[2] = "input.csv";
@@ -33,7 +40,7 @@ TEST(Arg, isReadOk)
 	EXPECT_EQ(READ, state);
 }
 
-TEST(Arg, isWriteOk)
+TEST(Arg, isWriteOk)//Test avec 13 arguments correct ecriture fichier
 {
 	argvWrite[1] = "--name";
 	argvWrite[2] = "toto";
@@ -52,9 +59,36 @@ TEST(Arg, isWriteOk)
 	EXPECT_EQ(WRITE, state);
 }
 
-TEST(Arg, isErrorOk)
+TEST(Arg, isErrorOk)//Test avec 2 arguments mais mauvaise syntaxe
 {
 	argvError[1] = "file";
-	ErrorArgTest.readArg(1, argvError, &state, &newdata);
+	ErrorArgTest.readArg(2, argvError, &state, &newdata);
+	EXPECT_EQ(ERROR, state);
+}
+
+TEST(Arg, isErrorSansArgOk)//Test sans arguments
+{
+	ErrorSansArg.readArg(1, argvError, &state, &newdata);
+	EXPECT_EQ(ERROR, state);
+}
+
+TEST(Arg, isErrorNoFileMentionnedOk)//Test avec 2 argument dont l'oublie du fichier csv
+{
+	argvErrorNoFile[1] = "--file";
+	ErrorNoFileMentionned.readArg(2, argvErrorNoFile, &state, &newdata);
+	EXPECT_EQ(ERROR, state);
+}
+
+TEST(Arg, isErrorHelpAskedOk)//Test avec 2 arguments dont l'oublie du fichier csv
+{
+	argvErrorHelpAsked[1] = "--help";
+	ErrorHelpAsked.readArg(2, argvErrorHelpAsked, &state, &newdata);
+	EXPECT_EQ(ERROR, state);
+}
+
+TEST(Arg, isErrorPhoneOnlyOk)//Test avec 2 arguments mais mauvais syntaxe
+{
+	argvErrorPhoneOnly[1] = "--phone";
+	ErrorPhoneOnly.readArg(2, argvErrorPhoneOnly, &state, &newdata);
 	EXPECT_EQ(ERROR, state);
 }
